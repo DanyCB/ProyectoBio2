1b7120d5ee70241796d1de20f3fd61d0ad06d4ca
Serra35 <73605882+Serra35@users.noreply.github.com>
2022-05-26 11:19:33 +0100

    Add files via upload

--- RPY.m
+++ RPY.m
@@ -1,3 +1,49 @@
+<<<<<<< HEAD
+% Inicia comunicación por puerto serie indicando el puerto y la 
+% velocidad de comunicación
+arduinoObj = serialport("COM3",9600); 
+
+% Indica el caracter con el que terminar una línea en puerto serie
+configureTerminator(arduinoObj,"CR/LF");
+% flush(arduinoObj);
+
+% Crea un struct donde se almacenan los datos del usuario y los va contando
+arduinoObj.UserData = struct("Data",[],"Count",1);
+
+% Escribe un caracter en el puerto serie
+write(arduinoObj,"s","string");
+
+% Lee la línea de datos que procede del puerto serie
+readline(arduinoObj);
+
+for i=1:10
+    readline(arduinoObj);
+end
+
+write(arduinoObj,"s","string");
+
+% Vacía el puerto serie
+flush(arduinoObj);
+readline(arduinoObj);
+readline(arduinoObj);
+readline(arduinoObj);
+arduinoObj.UserData.Data = [];
+
+% Para leer el puerto serie el número de veces deseado, llamando a la
+% función RPYData
+i=1;
+while(i<1000)
+    readRPYData(arduinoObj);
+    i=i+1;
+end
+
+% Imprime los datos que se van leyendo
+hold on;
+plot(arduinoObj.UserData.Data(:,1)); % Roll
+hold on;
+plot(arduinoObj.UserData.Data(:,2),'r'); % Pitch
+%plot(arduinoObj.USerData.Data(:,3),'g'); % Yaw
+=======
 % Inicia comunicación por puerto serie indicando el puerto y la 
 % velocidad de comunicación
 arduinoObj = serialport("/dev/cu.usbserial-0001",115200); 
@@ -41,3 +87,4 @@ plot(arduinoObj.USerData.Data(:,2),'r');
 %hold on;
 figure(3);
 plot(arduinoObj.USerData.Data(:,3),'t'); 
+>>>>>>> 41dbc5143c54dece0b9790011b86ea187417d48f
